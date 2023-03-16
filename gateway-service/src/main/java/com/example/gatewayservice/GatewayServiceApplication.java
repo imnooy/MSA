2dee17f25ff6c29703564e2c016e9b95cc63a4f6
yoonmihy <yoonmihy@naver.com>
2023-03-17 01:57:56 +0900

    add : load balance

--- gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
@@ -2,9 +2,11 @@ package com.example.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
+@EnableEurekaClient
 public class GatewayServiceApplication {
 
     public static void main(String[] args) {
--- my-first-service/src/main/java/com/example/myfirstservice/FirstServiceController.java
+++ my-first-service/src/main/java/com/example/myfirstservice/FirstServiceController.java
@@ -1,13 +1,25 @@
 package com.example.myfirstservice;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
+@Slf4j
 @RequestMapping("/first-service")
-public class FristServiceController {
+public class FirstServiceController {
+    Environment env;
+
+    @Autowired
+    public FirstServiceController(Environment env) { //생성자 통해 환경변수 주입받기
+        this.env=env;
+    }
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the Frist service";
@@ -20,7 +32,8 @@ public class FristServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi, there. This is a mesasge from First Service.";
+    public String check(HttpServletRequest request) {
+        log.info("Server port={}", request.getServerPort());
+        return String.format("Hi, there. This is a mesasge from First Service on PORT %s", env.getProperty("local.server.port"));
     }
 }
